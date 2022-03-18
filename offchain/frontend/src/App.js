aa59d121ce59d4b32d39b68532aa93686b3b50d5
dodadoa <maddodadoa@gmail.com>
2022-03-18 16:25:24 +0700

    adding buy website layout

--- offchain/frontend/src/App.js
+++ offchain/frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
+import Buy from "./pages/Buy"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navigator">
+        <Link className="nav-element" to="/"> Buy </Link>
+        <Link className="nav-element" to="/transactions"> Transactions </Link>
+        <Link className="nav-element" to="/chart"> Chart </Link>
+        <Link className="nav-element" to="/about"> About </Link>
+        <Link className="nav-element element-right" to="/login"> Login </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Buy />} />
+      </Routes>
     </div>
   );
 }
---
+++ offchain/frontend/src/pages/Buy.js
@@ -0,0 +1,49 @@
+import React from "react"
+import './Buy.css';
+
+const Buy = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Home</h1>
+      <div className="column">
+        <div className="row">
+          <div className="box-text color-boomer ">
+            <p className="box-text-content"> Boomer </p>
+          </div>
+          <div className="box-work">
+            
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="box-text color-gen-x">
+            <p className="box-text-content"> Generation X </p>
+          </div>
+          <div className="box-work">
+            
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="box-text color-gen-y">
+            <p className="box-text-content"> Millenial </p>
+          </div>
+          <div className="box-work">
+            
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="box-text color-gen-z">
+            <p className="box-text-content"> Generation Z </p>
+          </div>
+          <div className="box-work">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Buy;
