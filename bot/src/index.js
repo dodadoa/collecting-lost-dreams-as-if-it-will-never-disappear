6ef62a4ae282a5bd6c08befff82dc7e300d9644c
wasawat <wasawat@wasawats-MacBook-Pro.local>
2022-03-23 00:56:08 +0700

    make bot to endlessly undecidable to buy the NFT

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,22 +1,30 @@
-import { openBrowser, goto, $, dragAndDrop, closeBrowser, click, press, waitFor, currentURL, focus, text } from 'taiko'
+import { openBrowser, goto, write, closeBrowser, waitFor, click, $ } from 'taiko'
 
 
-const BUBBLE_DOT_COM = "https://dream.bbbbbbubble.com"
-const GOOGLE = "https://translate.google.com"
+const BUBBLE_DOT_COM = "dream.bbbbbbubble.com"
+const GOOGLE = "translate.google.com"
 
-(async () => {
+const running = async () => {
   try {
     await openBrowser({ headless: false, args: ["--start-fullscreen"] })
     await goto(BUBBLE_DOT_COM)
 
-    while(true) {
+    while (true) {
       await waitFor(500)
       await goto(GOOGLE)
-      await focus(textBox('Source text'))
+      await write('should I buy it ?')
+      /* listen button */
+      await click($('.r375lc'))
+      await goto(BUBBLE_DOT_COM)
+      for (let i = 0; i < 10; i++) {
+        await scrollDown(200)
+      }
     }
   } catch (error) {
     console.error(error);
   } finally {
     await closeBrowser();
   }
-})();
+}
+
+running()
